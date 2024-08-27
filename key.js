844047a2673e3ebbca1dc04df330d827eff59c82
KunnAOV <killervn08042006@gmail.com>
2024-08-27 23:11:28 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,7 +1,7 @@
 const validKeys = {
     mainKey: 'KunnAOV-2006xx', // Key chính đầu tiên
-    secondaryKey: '5mksvznQf0CCxGqA', // Key phụ đầu tiên
+    secondaryKey: 'oS8zZGQqM5N8tc8QD5VAnnqu', // Key phụ đầu tiên
     mainKey2: 'BANTOMOD-12355', // Key chính thứ hai
     mainKey3: 'GHOTSGREAM-12355', // Key chính thứ ba
-    secondaryKey2: 'hj7v6t0UqYTXPJXk' // Key phụ thứ hai
+    secondaryKey2: 'cwRdqdTF5AsjwQwV8JLqPkWU' // Key phụ thứ hai
 };
