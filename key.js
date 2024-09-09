9f88cffe8ee04a5a9922737a45cb1b25781f1d98
KunnAOV <killervn08042006@gmail.com>
2024-09-09 23:24:24 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,7 +1,7 @@
 const validKeys = {
     mainKey: 'KunnAOV-2006xx', // Key chính đầu tiên
-    secondaryKey: '4uQNOh6c1M', // Key phụ đầu tiên
+    secondaryKey: 'i3BXezhEpG6X', // Key phụ đầu tiên
     mainKey2: 'BANTOMOD-12355', // Key chính thứ hai
     mainKey3: 'GHOTSGREAM-12355', // Key chính thứ ba
-    secondaryKey2: '24YdSR3hS4' // Key phụ thứ hai
+    secondaryKey2: 'vPlvxtxEsG4H' // Key phụ thứ hai
 };
